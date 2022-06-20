c4b2af9bc94476308229757229e3f902d8a7075d
MahinulA <himahinulabid@gmail.com>
2022-06-20 23:01:26 +0600

    added some functions in webapp

--- app/js/index.js
+++ app/js/index.js
@@ -2,6 +2,26 @@ let journalHead;
 let journalAmount;
 let journalDescription;
 
+// add class to element functions(DRY code)
+let addClasstoElement = (selector, className) =>{
+    selector.classList.add(className);
+};
+
+let createRecord = ({getJournalHead, getJournalAmount, getJournalHeadDescription, appendToWhere}) =>{
+    let element = document.createElement('div');
+    addClasstoElement(element, "container");
+    addClasstoElement(element, "record-container__container");
+    addClasstoElement(element, "d-flex");
+    addClasstoElement(element, "mt-2");
+    element.innerHTML =`
+    <div class="item-container w-75 record-container__journalHeader">${getJournalHead}</div>
+    <div class="item-container w-25 record-container__money">${getJournalAmount}</div>
+    `;
+    document.querySelector(appendToWhere).appendChild(element);
+};
+
+// createRecord({getJournalHead: "some journal Head", getJournalAmount: "200"});
+
 // get data when someone clicks the button
 let getDataBtnInit = document.querySelectorAll(".get-data-btn");
 let getDataBtn;
@@ -20,10 +40,9 @@ for (let counter = 0; counter < getDataBtnInit.length; counter++) {
 
         // taking income and expense decision
         if (eventTextContent === "Income") {
-            console.log(`income`);
-
+            createRecord({getJournalHead: journalHead, getJournalAmount: journalAmount, getJournalHeadDescription: journalDescription, appendToWhere: ".income-category-journal"});
         } else if (eventTextContent === "Expense") {
-            console.log(`expense`);
+            createRecord({getJournalHead: journalHead, getJournalAmount: journalAmount, getJournalHeadDescription: journalDescription, appendToWhere: ".expense-category-journal"});
         } else {
             return;
         }
